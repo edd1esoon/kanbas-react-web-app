77a6dfe3bb9b89fb3dd47bf377b372cf8df05c46
Yiming Zhao <edd1esson@Edisons-MacBook-Air.local>
2024-12-08 20:39:45 -0500

    fix 3

--- src/Kanbas/Courses/Assignments/client.ts
+++ src/Kanbas/Courses/Assignments/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const ASSIGNMENTS_API = "https://kanbas-node-server-app.onrender.com/api/assignments";
+const ASSIGNMENTS_API = "https://kanbas-node-server-app-g3f6.onrender.com/api/assignments";
 
 export const fetchAssignments = async () => {
   const response = await axios.get(ASSIGNMENTS_API);
